0ccbc3077060804a3830986aa6501a2804b670ca
amandeep.sachdeva <amandeep.sachdeva@okoders.com>
2023-03-29 16:45:55 +0530

    project setup

---
+++ src/Router/MainRouter.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {Navbar} from '../Components/Navbar'
+
+
+
+ const MainRouter = () => {
+  return (
+    <div>
+      
+        <Navbar />
+        {/* <Routes>
+          <Route  path="/" element={<h1>Hello from Homepage</h1>} />
+          <Route  path="/home" element={<h1>Hello from Homepage</h1>} />
+          <Route  path="/aboutus" element={<h1>AboutUs</h1>} />
+          <Route  path="/ourservices" element={<h1>Services</h1>} />
+          <Route  path="/works" element={<h1>Works</h1>} />
+          <Route  path="/blogs" element={<h1>Blogs</h1>} />
+          <Route  path="/news" element={<h1>News</h1>} />
+        </Routes> */}
+      
+     </div>
+  )
+}
+
+export default MainRouter
