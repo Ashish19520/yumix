28039286d4f92c7a69b95bd8a8bf5b1c87962896
amandeep.sachdeva <amandeep.sachdeva@okoders.com>
2023-03-30 12:08:45 +0530

    navbar ui

--- src/Router/MainRouter.tsx
+++ src/Router/MainRouter.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import {Navbar} from '../Components/Navbar'
+import { Navbar } from '../Components/Navbar'
+import { Homepage } from '../Pages/Homepage';
 
 
 
- const MainRouter = () => {
+const MainRouter = () => {
   return (
     <div>
       
-        <Navbar />
-        {/* <Routes>
-          <Route  path="/" element={<h1>Hello from Homepage</h1>} />
-          <Route  path="/home" element={<h1>Hello from Homepage</h1>} />
-          <Route  path="/aboutus" element={<h1>AboutUs</h1>} />
-          <Route  path="/ourservices" element={<h1>Services</h1>} />
-          <Route  path="/works" element={<h1>Works</h1>} />
-          <Route  path="/blogs" element={<h1>Blogs</h1>} />
-          <Route  path="/news" element={<h1>News</h1>} />
-        </Routes> */}
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<h1>Hello from Homepage</h1>} />
+        <Route path="/aboutus" element={<h1>AboutUs</h1>} />
+        <Route path="/ourservices" element={<h1>Services</h1>} />
+        <Route path="/works" element={<h1>Works</h1>} />
+        <Route path="/blogs" element={<h1>Blogs</h1>} />
+        <Route path="/news" element={<h1>News</h1>} />
+      </Routes>
       
-     </div>
+    </div>
   )
 }
 
