122b26c7de4413746646ea2d9274680ee566d40a
ashish19520 <you@example.com>
2023-05-16 12:38:02 +0530

    responsive listing module

--- src/Container/ListingCalculator/style.ts
+++ src/Container/ListingCalculator/style.ts
@@ -1,165 +1,199 @@
 import { makeStyles } from '@material-ui/core';
 
 export default makeStyles((theme:any) => ({
-    wrapper:{
+    wrapper: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'flex-start',
         background: "#FFFCFC",
-    },
-    img:{
-        width:"30%",
-    },
-    innerWrapper:{
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column',
+          alignItems: 'center',
+        },
+      },
+      img: {
+        width: "30%",
+        [theme.breakpoints.down('sm')]: {
+          width: "100%",
+        },
+      },
+      innerWrapper: {
         display: 'flex',
-        flexWrap:'wrap',
+        flexWrap: 'wrap',
         flexDirection: 'column',
         justifyContent: 'flex-start',
-        gap:"30px",
-        alignIems:"center",
+        gap: "30px",
+        alignItems: "center",
         background: "#FFFCFC",
-    },
-    items:{
+      },
+      items: {
         display: 'flex',
         flexWrap: 'wrap',
         flexDirection: 'row',
-        justifyContent:"space-between",
-        gap:'80px',
+        justifyContent: "space-between",
+        gap: '80px',
         background: "#FFFCFC",
-    },
-    twoContainer:{
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column',
+          alignItems: 'center',
+        },
+      },
+      twoContainer: {
         display: 'flex',
-        flexWrap:'wrap',
-        justifyContent:"center",
+        flexWrap: 'wrap',
+        justifyContent: "center",
         alignItems: "center",
         gap: "120px",
         background: "#FFFCFC",
-        
-
-    },
-    innerTwoContainer:{
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '30px',
+        },
+      },
+      innerTwoContainer: {
         display: 'flex',
-        flexWrap:'wrap',
+        flexWrap: 'wrap',
         flexDirection: 'column',
-        justifyContent:"center",
+        justifyContent: "center",
         alignItems: "center",
-        gap:"40px",
+        gap: "40px",
         background: "#FFFFFF",
-        padding:"30px",
-        
+        padding: "30px",
         /* Card Shadow */
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.08)",
-                bordeRadius: "12px",
-    },
-    innerThirdContainer:{
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.08)",
+        borderRadius: "12px",
+        [theme.breakpoints.down('sm')]: {
+          padding: "20px",
+        },
+      },
+      innerThirdContainer: {
         display: 'flex',
-        flexWrap:'wrap',
+        flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'center',
-        gap:"30px",
-       
-    },
-    fourthContainer:{
+        gap: "30px",
+      },
+      fourthContainer: {
         display: 'flex',
         flexDirection: 'column',
-        flexWrap:"wrap",
+        flexWrap: "wrap",
         justifyContent: 'flex-start',
-    },
-    innerOneFourth:{
+      },
+      innerOneFourth: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         padding: "40px",
         gap: "48px",
-
-        // position: absolute;
-        // width: 1151px;
-        // height: 101px;
-        // left: 64px;
-        // top: 114px;
-
         background: "#FFFFFF",
         border: "1px solid #FFFCFC",
         /* Card Shadow */
-        boxShadow:" 0px 4px 4px rgba(0, 0, 0, 0.08)",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.08)",
         borderRadius: "16px",
         marginBottom: "50px",
-    },
-    btnFBACalculate:{
-        background:"#5956e9",
-        color:"#e0e0e0",
-        borderRadius:"10px",
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column',
+          padding: "20px",
+        },
+      },
+      btnFBACalculate: {
+        background: "#5956e9",
+        color: "#e0e0e0",
+        borderRadius: "10px",
         padding: "10px 25px",
-        fontFamily:"Poppins",
-        fontSize:"12px",
-        lineHeight:"18px",
-        letterSpacing:"0.001em",
-        textAlign:"center",    
-    },
-    fifthContainer:{
+        fontFamily: "Poppins",
+        fontSize: "12px",
+        lineHeight: "18px",
+        letterSpacing: "0.001em",
+        textAlign: "center",
+      },
+      fifthContainer: {
         display: "flex",
         flexDirection: "column",
+        flexWrap:"wrap",
         justifyContent: "center",
         alignItems: "center",
         padding: "0px",
         gap: "64px",
-        margin:"0 auto",
-    },
-    innerFifthContainer:{
-        display:"flex",
+        margin: "0 auto",
+        
+        [theme.breakpoints.down('sm')]: {
+          gap: "30px",
+          marginBottom:"250px",
+        },
+      },
+      innerFifthContainer: {
+        display: "flex",
         justifyContent: "flex-start",
         flexDirection: "column",
         width: "956px",
         height: "125px",
-    },
-    innersecondFifthContainer:{
+        [theme.breakpoints.down('sm')]: {
+          width: "100%",
+        },
+      },
+      innersecondFifthContainer: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
         padding: "0px",
-        gap:"20px"
-    },
-    greenGood:{
-        width:"108px",
-        height:"37px",
-        left:"79px",
-        radius:"8px",
-        padding:"5px 0",
-        gap:"10px",
-        background:"#D9ECCC",
-        textAlign:"center",  
-        tex:"center",
-    },
-    redGood:{
-        width:"108px",
-        height:"37px",
-        left:"79px",
-        radius:"8px",
-        padding:"5px 0",
-        gap:"10px",
-        background:"#ECCCCC",
-        textAlign:"center",  
-        tex:"center",
-    },
-    innerSeventh:{
-        marginTop:"20px",
+        // flexWrap:'wrap',
+        gap: "20px",
+        [theme.breakpoints.down('sm')]: {
+            width: "100%",
+            flexDirection: "column-reverse",
+          },
+      },
+      greenGood: {
+        width: "108px",
+        height: "37px",
+        left: "79px",
+        borderRadius: "8px",
+        padding: "5px 0",
+        gap: "10px",
+        background: "#D9ECCC",
+        textAlign: "center",
+        text: "center",
+      },
+      redGood: {
+        width: "108px",
+        height: "37px",
+        left: "79px",
+        borderRadius: "8px",
+        padding: "5px 0",
+        gap: "10px",
+        background: "#ECCCCC",
+        textAlign: "center",
+        text: "center",
+      },
+      innerSeventh: {
+        marginTop: "20px",
         display: "flex",
         flexDirection: "row",
-        flexWrap:"wrap",
-        gap:"-40px"
-    },
-    sixthContainer:{
+        flexWrap: "wrap",
+        gap: "-40px",
+      },
+      sixthContainer: {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+        flexWrap: "wrap",
         padding: "0px",
         gap: "0",
-    },
-    selectors:{
-        padding:"0px",
-        margin:"0px",
-    },
+      },
+      selectors: {
+       minWidth: "850px", 
+       width:"100%",
+       display: "flex",
+       flexDirection: "column",
+       flexWrap: "wrap",
+       [theme.breakpoints.down('sm')]: {
+       minWidth: "100vw",
+      },
+        
+      },
     
 }));
 
