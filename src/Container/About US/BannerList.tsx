76dfb18e5af924a00f2def210ac977b01cec38e9
Vikash Kumar <vikash.kumar@okoders.com>
2023-07-03 15:18:22 +0530

    change title color on some frequency

--- src/Container/About US/BannerList.tsx
+++ src/Container/About US/BannerList.tsx
@@ -1,7 +1,45 @@
 import { Box, Grid, Typography } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function BannerList() {
+  const[listColor,setlistColor] = useState("#3A3B44");
+  const[advertiseColor,setadvertiseColor] = useState("#828490");
+  const[growColor,setGrowColor] = useState("#828490");
+
+
+  const handleColorFunction=()=>{
+    setTimeout(()=>{
+      setlistColor("#828490");
+      setadvertiseColor("#3A3B44");
+      setGrowColor("#828490");
+    },2000)
+
+    setTimeout(()=>{
+      setlistColor("#828490");
+      setadvertiseColor("#828490");
+      setGrowColor("#3A3B44");
+    },4000)
+
+    setTimeout(()=>{
+      setlistColor("#3A3B44");
+      setadvertiseColor("#828490");
+      setGrowColor("#828490");
+      // setIsCycleComplete(true);
+    },6000)
+
+  }
+
+  useEffect(()=>{
+    handleColorFunction()
+  },[]);
+
+  if(listColor === "#828490" && growColor ==="#3A3B44" ){
+    setTimeout(()=>{
+      handleColorFunction();
+    },2000)
+   
+  }
+
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -16,13 +54,32 @@ export default function BannerList() {
     const hiddenElements = document.querySelectorAll(".bannerList_hidden");
     hiddenElements.forEach((el) => observer.observe(el));
   }, []);
+
+
+  
+
   return (
     <Box className="bannerList_hidden" style={{maxWidth:"1200px", margin:"auto", padding:"80px 40px"}}>
       <Grid position="relative" container alignItems="center" style={{borderRadius:"40px", backgroundColor:"#EFF5FC", padding:"40px 60px"}}>
         <Grid item xs={12} md={8} sx={{paddingTop:{xs:"140px", md:"0px"}}}>
-          <Typography fontSize="32px" fontFamily="Poppins-Medium" color="#3A3B44">
-            List ∙ <span style={{color:'#828490'}}>Advertise ∙ Grow</span>
+          <div style={{display:"flex",gap:10}} >
+
+          <Typography fontSize="32px" fontFamily="Poppins-Medium" style={{color:`${listColor}`}} >
+            List
           </Typography>
+
+          <Typography fontSize="32px" fontFamily="Poppins-Medium" color="#828490" style={{color:`${advertiseColor}`}} >
+          ∙ Advertise 
+          </Typography>
+
+          <Typography fontSize="32px" fontFamily="Poppins-Medium" color="#3A3B44" style={{color:`${growColor}`}} >
+          ∙ Grow
+          </Typography>
+
+          </div>
+          {/* <Typography fontSize="32px" fontFamily="Poppins-Medium" color="#3A3B44">
+            List ∙ <span style={{color:'#828490'}}>Advertise ∙ Grow</span>
+          </Typography> */}
           <Typography fontSize="16px" fontFamily="Montserrat" color="#828490" style={{marginTop:"15px"}}>
             There are more than 30 Million product listing on Amazon Platform. Beat your FBA competitors with our listing audit and optimization service.
           </Typography>
